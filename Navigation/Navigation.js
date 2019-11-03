f497d3fbcf8d1b8ce7a7d636ae3997e8325b7dac
tmaulon <thomas.maulon@gmail.com>
2019-11-03 16:18:28 +0100

    eject Movies And Me App

---
+++ Navigation/Navigation.js
@@ -0,0 +1,86 @@
+// Navigation/Navigations.js
+
+import React from "react";
+import { StyleSheet, Image } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import Search from "../Components/Search";
+import FilmDetail from "../Components/FilmDetail";
+import Favorites from "../Components/Favorites";
+import Test from "../Components/Test";
+
+const SearchStackNavigator = createStackNavigator({
+  Search: {
+    screen: Search,
+    navigationOptions: {
+      title: "Rechercher"
+    }
+  },
+  FilmDetail: {
+    screen: FilmDetail
+  }
+});
+
+const FavoritesStackNavigator = createStackNavigator({
+  Favorites: {
+    screen: Favorites,
+    navigationOptions: {
+      title: "Favoris"
+    }
+  },
+  FilmDetail: {
+    screen: FilmDetail
+  }
+});
+
+const MoviesTabNavigator = createBottomTabNavigator(
+  {
+    // Test: {
+    //   screen: Test
+    // },
+    Search: {
+      screen: SearchStackNavigator,
+      navigationOptions: {
+        tabBarIcon: () => {
+          return (
+            <Image
+              source={require("../Images/ic_search.png")}
+              style={styles.icon}
+            />
+          );
+        }
+      }
+    },
+    Favorites: {
+      screen: FavoritesStackNavigator,
+      navigationOptions: {
+        tabBarIcon: () => {
+          return (
+            <Image
+              source={require("../Images/ic_favorite.png")}
+              style={styles.icon}
+            />
+          );
+        }
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeBackgroundColor: "#DDDDDD",
+      inactiveBackgroundColor: "#FFFFFF",
+      showLabel: false,
+      showIcon: true
+    }
+  }
+);
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 30,
+    height: 30
+  }
+});
+
+export default createAppContainer(MoviesTabNavigator);
