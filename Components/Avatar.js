9c95b96666610ac321ba62323b3d6d869eb0a571
tmaulon <thomas.maulon@gmail.com>
2019-11-03 17:19:45 +0100

    implement avatar with ImagePicker

--- Components/Avatar.js
+++ Components/Avatar.js
@@ -1,29 +1,42 @@
 // Components/Avatar.js
 
-import React from 'react'
-import { StyleSheet, Image, TouchableOpacity } from 'react-native'
+import React from 'react';
+import {StyleSheet, Image, TouchableOpacity} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 
 class Avatar extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      avatar: require('../Images/ic_tag_faces.png')
-    }
+      avatar: require('../Images/ic_tag_faces.png'),
+    };
   }
 
   _avatarClicked() {
     // Ici nous appellerons la librairie react-native-image-picker pour récupérer un avatar
+    ImagePicker.showImagePicker({}, response => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else {
+        console.log('Photo => ', response.uri);
+        let requireSource = {uri: response.uri};
+        this.setState({
+          avatar: requireSource,
+        });
+      }
+    });
   }
 
   render() {
-    return(
+    return (
       <TouchableOpacity
         style={styles.touchableOpacity}
         onPress={this._avatarClicked}>
-          <Image style={styles.avatar} source={this.state.avatar} />
+        <Image style={styles.avatar} source={this.state.avatar} />
       </TouchableOpacity>
-    )
+    );
   }
 }
 
@@ -33,15 +46,15 @@ const styles = StyleSheet.create({
     width: 100, // Pensez bien à définir une largeur ici, sinon toute la largeur de l'écran sera cliquable
     height: 100,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   avatar: {
     width: 100,
     height: 100,
     borderRadius: 50,
     borderColor: '#9B9B9B',
-    borderWidth: 2
-  }
-})
+    borderWidth: 2,
+  },
+});
 
-export default Avatar
+export default Avatar;
