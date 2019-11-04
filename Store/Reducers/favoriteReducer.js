045c30f79a96e7ce840d3e69808c883ce212679d
tmaulon <thomas.maulon@gmail.com>
2019-11-04 08:59:04 +0100

    refactor favoriteReducer

--- Store/Reducers/favoriteReducer.js
+++ Store/Reducers/favoriteReducer.js
@@ -1,27 +1,27 @@
 // Store/Reducers/favoriteReducer.js
 
-const initialState = { favoritesFilm: [] };
+const initialState = {favoritesFilm: []};
 
 const toggleFavorite = (state = initialState, action) => {
   let nextState;
   switch (action.type) {
-    case "TOGGLE_FAVORITE":
+    case 'TOGGLE_FAVORITE':
       const favoriteFilmIndex = state.favoritesFilm.findIndex(
-        item => item.id === action.value.id
+        item => item.id === action.value.id,
       );
       if (favoriteFilmIndex !== -1) {
         // Le film est déjà dans les favoris, on le supprime de la liste
         nextState = {
           ...state,
           favoritesFilm: state.favoritesFilm.filter(
-            (item, index) => index !== favoriteFilmIndex
-          )
+            (item, index) => index !== favoriteFilmIndex,
+          ),
         };
       } else {
         // Le film n'est pas dans les films favoris, on l'ajoute à la liste
         nextState = {
           ...state,
-          favoritesFilm: [...state.favoritesFilm, action.value]
+          favoritesFilm: [...state.favoritesFilm, action.value],
         };
       }
       return nextState || state;
